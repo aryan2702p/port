e7281d859cee8cb9612f663ad2fb90a91c023d3b
aryan2702p <arynmi02@gmail.com>
2023-08-20 19:43:55 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import About from './components/about/about'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import Exp from './components/experience/experience'
+import Portfolio from './components/portfolio/portfolio'
+import Services from './components/services/services'
+import Test from './components/testimonials/testimonial'
+import Contact from './components/contacts/contact'
+import Footer from './components/footer/footer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+    return  (
+        <>
+        <Header />
+        <Nav />
+       <About />
+       <Exp />
+       <Services />
+       <Portfolio />
+       <Test />
+       <Contact />
+       <Footer />
 
+        </>
+    );
+   
+    
+};
 export default App;
